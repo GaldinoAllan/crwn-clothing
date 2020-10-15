bfc2f56da9cec51c19f6684daac5cad6c013da07
Allan Galdino <allan.g.galdino@outlook.com>
2020-10-15 18:56:48 -0300

    sign in completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from './pages/Homepage';
 import ShopPage from './pages/ShopPage';
 import SignInSignUp from './pages/SignInSignUp';
 import Header from './components/Header';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -21,9 +21,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
