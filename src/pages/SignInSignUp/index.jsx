bb588b54857670d145720b8ef201dc1c8befb35d
Allan Galdino <allan.g.galdino@outlook.com>
2020-10-15 19:14:03 -0300

    sign up component done without firebase

--- src/pages/SignInSignUp/index.jsx
+++ src/pages/SignInSignUp/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/SignIn';
+import SignUp from '../../components/SignUp';
 
 import './styles.scss';
 
 const SignInSignUp = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
