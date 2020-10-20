50d9f417cfe15ba37f6c78d495d68904dccd37b9
Allan Galdino <allan.g.galdino@outlook.com>
2020-10-20 09:19:35 -0300

    stripe library integrated for payment purposes

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../redux/cart/cart.selectors'
 
 import CheckoutItem from '../components/CheckoutItem';
+import StripeCheckoutButton from '../components/StripeButton';
 
 import '../sass/pages/Checkout.scss';
 
@@ -33,6 +34,12 @@ const Checkout = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit cart for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 09/23 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
