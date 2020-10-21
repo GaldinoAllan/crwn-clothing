4baa0a4640145d1fed72ca31bfba6fc3ecd9ff7c
Allan Galdino <allan.g.galdino@outlook.com>
2020-10-21 09:21:28 -0300

    unssetting env variables to production

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,24 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+const REACT_APP_API_KEY = "AIzaSyBEiH36LDfBrI-FgQ9EF7eNYHoUYgjYHyU";
+const REACT_APP_AUTH_DOMAIN = "crwn-clothing-data-base.firebaseapp.com";
+const REACT_APP_DATABASE_URL = "https://crwn-clothing-data-base.firebaseio.com";
+const REACT_APP_PROJECT_ID = "crwn-clothing-data-base";
+const REACT_APP_STORAGE_BUCKET = "crwn-clothing-data-base.appspot.com";
+const REACT_APP_MESSAGING_SENDER_ID = "979213854379";
+const REACT_APP_APP_ID = "1:979213854379:web:e838886b7a7137430d79ee";
+const REACT_APP_MEASUREMENT_ID = "G-EDTZHNGJH5";
+
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
